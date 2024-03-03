36271d16338e0e8740b823334295fe83ab1d0e29
Adriano Vieira <adriano@aoro.ai>
2024-03-03 15:55:19 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Adriano Vieira on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
